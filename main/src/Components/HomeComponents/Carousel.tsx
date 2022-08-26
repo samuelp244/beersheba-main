40e11d5843b70aabf9f288dc866f75c8fb820d4c
samuelp244 <samuelp244@gmail.com>
2022-08-26 19:57:08 +0530

    ls

--- main/src/Components/HomeComponents/Carousel.tsx
+++ main/src/Components/HomeComponents/Carousel.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import slide1 from "../../assets/home-slidebar-pictures/edited_HOUSE_OF_BEATITUDES.png"
+import slide1 from "../../assets/home-slidebar-pictures/slide01.JPG"
 // import slide2 from "../assets/home-slidebar-pictures/Beersheba.png";
-import slide3 from "../../assets/home-slidebar-pictures/Shalem_edited.jpg";
+// import slide3 from "../../assets/home-slidebar-pictures/Shalem_edited.jpg";
 import useMediaQuery from '../../Hooks/useMediaQuery';
 
 const BeershebaCarousel = () => {
@@ -11,7 +11,7 @@ const BeershebaCarousel = () => {
             <div id="carouselExampleIndicators" className="carousel slide shadow-lg  mb-5 bg-white" data-bs-ride="carousel" data-bs-interval="3000">
                 <div className="carousel-indicators">
                     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                    {/* <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button> */}
                     {/* <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button> */}
                 </div>
                 <div className="carousel-inner">
@@ -21,9 +21,9 @@ const BeershebaCarousel = () => {
                     {/* <div className="carousel-item">
                         <img src={slide2} className="d-block max-h-96 w-100" alt="..."/>
                     </div> */}
-                    <div className="carousel-item">
+                    {/* <div className="carousel-item">
                         <img src={slide3} className="d-block max-h-96 w-100" alt="..."/>
-                    </div>
+                    </div> */}
                 </div>
                 <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
                     <span className="carousel-control-prev-icon" aria-hidden="true"></span>
--- main/src/types/index.d.ts
+++ main/src/types/index.d.ts
@@ -1,4 +1,5 @@
 declare module "*.jpg";
+declare module "*.JPG";
 declare module "*.png";
 declare module "*.jpeg";
 declare module "*.gif";
