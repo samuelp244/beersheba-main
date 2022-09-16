cc9ed664d5e4fc2fcfd4d2c81f417839fa87e1e1
samuelp244 <samuelp244@gmail.com>
2022-09-16 15:55:52 +0530

    avoiding private videos

--- main/src/Components/HomeComponents/Carousel.tsx
+++ main/src/Components/HomeComponents/Carousel.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import slide1 from "../../assets/home-slidebar-pictures/image-2.jpg"
+// import slide1 from "../../assets/home-slidebar-pictures/image-2.jpg"t
 import slide2 from "../../assets/home-slidebar-pictures/lord.JPG";
 import slide3 from "../../assets/home-slidebar-pictures/livenew.png";
 import useMediaQuery from '../../Hooks/useMediaQuery';
@@ -13,18 +13,18 @@ const BeershebaCarousel = () => {
                 <div className="carousel-indicators">
                     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
                     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button> 
-                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                    {/* <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button> */}
                 </div>
                 <div className="carousel-inner">
                     <div className="carousel-item active">
-                        <img src={slide1} className="d-block  max-h-96 w-100" alt="..."/>
-                    </div>
-                    <div className="carousel-item">
-                        <img src={slide2} className="d-block max-h-96 w-100" alt="..."/>
+                        <img src={slide2} className="d-block  max-h-96 w-100" alt="..."/>
                     </div>
                     <Link to="/live" className="carousel-item">
                         <img src={slide3} className="d-block max-h-96 w-100" alt="..."/>
                     </Link>
+                    {/* <Link to="/live" className="carousel-item">
+                        <img src={slide3} className="d-block max-h-96 w-100" alt="..."/>
+                    </Link> */}
                 </div>
                 <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
                     <span className="carousel-control-prev-icon" aria-hidden="true"></span>
--- main/src/Components/YTComponents/recentPlaylist.tsx
+++ main/src/Components/YTComponents/recentPlaylist.tsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom';
 import {IoIosArrowDropright} from "react-icons/io"
 import RecentPlaylistItemsList from '../../api/QueryComponents/recentPlaylistItemsList';
 import { useAllPlaylistsData } from '../../api/queries';
+import { mongoPlaylistdata } from '../../types/apiResponseTypes';
 
 const RecentPlaylist = () => {
     const { data } = useAllPlaylistsData()
+    const [currentData,setCurrentData] = useState<mongoPlaylistdata>()
+    useEffect(()=>{
+        if(data!==undefined){
+            for(let i=0;i<data?.length;i++){
+                if (data[i].items[0].snippet.title!=="Private video") {
+                    setCurrentData(data[i]);
+                    break;
+                }
+            }
+        }
+    },[data])
     
 
   return (
@@ -14,11 +26,11 @@ const RecentPlaylist = () => {
             <h1 className='text-lg font-bold'>Recent Sermon</h1>
         </div>
         <div >
-            <h1 className='text-lg mb-4 font-medium '>{data? data[0].PlaylistTitle:null}</h1>
-            {data?<RecentPlaylistItemsList playlistId={data? data[0].PlaylistId:''} />:null}
+            <h1 className='text-lg mb-4 font-medium '>{currentData? currentData.PlaylistTitle:null}</h1>
+            {data?<RecentPlaylistItemsList playlistId={currentData? currentData.PlaylistId:''} />:null}
         </div>
         <div className='flex justify-end'>
-            <Link to={`/series/${data? data[0].PlaylistId:''}`}><IoIosArrowDropright size={"30px"} color={"rgb(59 130 246)"} /> </Link>
+            <Link to={`/series/${currentData? currentData.PlaylistId:''}`}><IoIosArrowDropright size={"30px"} color={"rgb(59 130 246)"} /> </Link>
         </div>
     </div>
   )
